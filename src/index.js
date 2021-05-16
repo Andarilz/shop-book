918f2cfdc5a7a6fb166599085330cc28b43400ba
Kuroro Lucifer <isgrammar@gmail.com>
2021-05-16 16:38:34 +0300

    route1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react'
-import App from 'components/app'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux' //создали редакс провайдера
+import {BrowserRouter} from "react-router-dom";
+import ErrorBoundary from 'components/error-boundary'
+import BookStoreService from 'services/book-store' //создали getBooks
+import {BookStoreServiceProvider} from 'components/context' //создали контекст реактовский
+import store from 'store' //редаксовский стор
+import App from 'components/app'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+const bookStoreService = new BookStoreService()
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ErrorBoundary>
+            <BookStoreServiceProvider value={bookStoreService}>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </BookStoreServiceProvider>
+        </ErrorBoundary>
+    </Provider>
+
+    , document.getElementById('root'))
