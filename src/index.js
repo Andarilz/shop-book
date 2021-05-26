336cf5a759cd325223f3b4d8caa5ab1b1a103427
Kuroro Lucifer <isgrammar@gmail.com>
2021-05-26 19:57:15 +0300

    redux roote

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux' //создали редакс провайдера
 import {BrowserRouter} from "react-router-dom";
 import ErrorBoundary from 'components/error-boundary'
-import BookStoreService from 'services/book-store' //создали getBooks
-import {BookStoreServiceProvider} from 'components/context' //создали контекст реактовский
+import BookStore from 'services/book-store' //создали getBooks
+import {BookStoreService} from 'components/context'
 import store from 'store' //редаксовский стор
 import App from 'components/app'
 
-const bookStoreService = new BookStoreService()
+const bookStoreService = new BookStore()
 
 ReactDOM.render(
     <Provider store={store}>
         <ErrorBoundary>
-            <BookStoreServiceProvider value={bookStoreService}>
+            <BookStoreService.Provider value={bookStoreService}>
                 <BrowserRouter>
                     <App/>
                 </BrowserRouter>
-            </BookStoreServiceProvider>
+            </BookStoreService.Provider>
         </ErrorBoundary>
     </Provider>
 
